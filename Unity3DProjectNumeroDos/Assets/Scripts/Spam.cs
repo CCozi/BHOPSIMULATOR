af1724e587e5d0a52fb7e42921dfa9178ce2246f
CCozi <matthewledgard@gmail.com>
2016-04-22 08:44:40 -0700

    First Upload

---
+++ Unity3DProjectNumeroDos/Assets/Scripts/Spam.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spam : MonoBehaviour {
+	void Update() {
+		if (Input.GetKeyDown("space"))
+			print("space key was pressed");
+		
+	}
+}

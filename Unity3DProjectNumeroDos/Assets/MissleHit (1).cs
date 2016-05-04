ffd912f3898a962ec0331da8c977bb04f24fe147
CCozi <matthewledgard@gmail.com>
2016-05-04 08:23:10 -0700

    LastVersion
    RIP

---
+++ Unity3DProjectNumeroDos/Assets/MissleHit (1).cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissleHit : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log ("hit missle");
+
+		Rigidbody2D otherBody = other.GetComponent<Rigidbody2D> ();
+		otherBody.velocity = new Vector2 (-400, 200);
+		}
+}
